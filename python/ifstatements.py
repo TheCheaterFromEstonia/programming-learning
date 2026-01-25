592653bd5005cfb9b203a2289e4ba944830544d6
xYcBaq2 <123418333+TheCheaterFromEstonia@users.noreply.github.com>
2026-01-25 21:29:45 +0200

    Update ifstatements.py

--- python/ifstatements.py
+++ python/ifstatements.py
@@ -2,5 +2,9 @@
 age = int(input("How old are you?: "))
 if age >= 18:
     print("You are an adult.")
+    
+elif age <= 0: 
+    print("You arent born yet.")
+
 else:
     print("You are underage.")
