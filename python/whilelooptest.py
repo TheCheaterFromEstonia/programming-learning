9e1c7764c2d472e25efa7ad1f71fe06419b13e34
TheCheaterFromEstonia <hellna>
2026-02-05 21:34:38 +0200

    lol how was that broken

--- python/whilelooptest.py
+++ python/whilelooptest.py
@@ -1,5 +1,7 @@
+#the first line triggers at the start
 pin = int(input("Enter PIN: "))
+#If the correct PIN is entered, the loop will stop, exiting the program. Though if the wrong PIN is entered, the loop will trigger line 5, prompting me to input again.
 while pin != 5431:
-    pin = input("Wrong PIN. Enter your PIN: ")
-if pin == 5431:
-    print("Access granted!")
+    pin = int(input("Wrong PIN. Enter your PIN: "))
+    
+
